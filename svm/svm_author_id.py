3c15e2cea5be39801958fbad016ed5fabfc0ed29
Puneet-Shivanand <1991.puneet@gmail.com>
2015-06-02 16:19:30 +0530

    Update svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -26,7 +26,46 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from class_vis import prettyPicture
+from prep_terrain_data import makeTerrainData
 
+import matplotlib.pyplot as plt
+import copy
+import numpy as np
+import pylab as pl
+
+
+features_train, labels_train, features_test, labels_test = makeTerrainData()
+
+
+########################## SVM #################################
+### we handle the import statement and SVC creation for you here
+from sklearn.svm import SVC
+from sklearn import svm
+clf = svm.SVC()
+
+
+#### now your job is to fit the classifier
+#### using the training features/labels, and to
+#### make a set of predictions on the test data
+
+clf.fit(features_train, labels_train)
+SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0, degree=3,
+gamma=0.0, kernel='rbf', max_iter=-1, probability=False, random_state=None,
+shrinking=True, tol=0.001, verbose=False)
+
+#### store your predictions in a list named pred
+
+pred = clf.predict(features_test)
+
+
+
+from sklearn.metrics import accuracy_score
+#acc = accuracy_score(pred, labels_test)
+
+def submitAccuracy():
+    acc = accuracy_score(pred, labels_test)
+    return acc
 #########################################################
 
 
